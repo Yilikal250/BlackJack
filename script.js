89303937a7eac1a3f99689155d9abf94260184b0
Yilikal250 <teshomeyilikal250@gmail.com>
2022-11-23 19:46:15 +0300

    added textContent to buttons

---
+++ script.js
@@ -0,0 +1,37 @@
+let firstCard=10
+let secondCard=11
+
+let cards=[firstCard, secondCard]
+let sum= firstCard + secondCard 
+let hasBlackJack= false
+let isAlive= true
+let message=""
+let messageEl = document.getElementById("message-el")
+let sumEl=document.querySelector("#sum-el")
+let cardsEl=document.querySelector("#card-el")
+function renderGame(){ 
+cardsEl.textContent="The cards are: " + " " + cards[0] + " ," + cards[1] + " " 
+sumEl.textContent="Sum: " + sum      
+if(sum <= 21){
+        message="Do you want to draw a new card?"
+        }
+        else if(sum === 21){
+            message="Wohoo! You've got BlackJack! "
+            hasBlackJack= true
+            }
+        else {
+            message="You're out of the game! "
+            isAlive= false
+        } 
+messageEl.textContent=message
+}
+function startGame(){
+    renderGame()
+}
+let card=5
+function newCard(){  
+sum += card
+cards.push(card)
+console.log(cards)
+renderGame()   
+}
